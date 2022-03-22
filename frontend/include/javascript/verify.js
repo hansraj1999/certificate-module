6f35984b9aace572b1e3394b77636dc343586349
hansraj1999 <deghun@gmail.com>
2022-03-23 03:43:24 +0530

    added session management on home page after login and logout javascript

--- frontend/include/javascript/verify.js
+++ frontend/include/javascript/verify.js
@@ -1,8 +1,13 @@
-function verify(argument) {
+function verify() {
 	var token = sessionStorage.getItem("token");
 	console.log(sessionStorage.getItem("token"));
-	console.log(token);
+//	console.log(token);
 	if (token === null){
 		window.location.replace("./login.html");
 	}
 }
+function logout() {
+      sessionStorage.clear();
+      alert('You are about to Logout');
+      window.location.href='index.html'
+    }
