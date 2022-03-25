81bde7a6b1c6491f8a09d8ed2d096fb1a053bc51
hansraj1999 <deghun@gmail.com>
2022-03-25 20:17:10 +0530

    app.yaml file for google cloud

--- database.py
+++ database.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import _mysql_connector
 import sqlite3
 
-SQLALCHAMY_DB_URL = 'sqlite:///./data.db'
-engine = create_engine(SQLALCHAMY_DB_URL, connect_args={'check_same_thread': False})
+#SQLALCHAMY_DB_URL = 'sqlite:///./data.db'
+#engine = create_engine(SQLALCHAMY_DB_URL, connect_args={'check_same_thread': False})
 
-#SQLALCHAMY_DB_URL = 'mysql+mysqlconnector://root:root@localhost:3306/main'
-# engine = create_engine(SQLALCHAMY_DB_URL)
+SQLALCHAMY_DB_URL = 'mysql+mysqlconnector://root:root@localhost:3306/main'
+engine = create_engine(SQLALCHAMY_DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
